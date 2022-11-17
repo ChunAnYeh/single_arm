351695f3b74cec0aeabad5f4d8bbf7232c1abd16
ChunanYeh <tw273414@gmail.com>
2022-11-17 22:43:17 +0800

    “”

---
+++ build/single_arm_v2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/iclab/test_ws/src/single_arm_v2
+# Build directory: /home/iclab/test_ws/build/single_arm_v2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
